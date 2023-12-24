3b6f1976de62364420c46ed9bfcf8e874bfdd777
ravindrakaya <kayaravindra118@gmail.com>
2023-12-24 06:04:25 +0000

    solution

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,72 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken} `,
+      },
+    }
+    const response = await fetch(apiUrl, options)
+    // console.log(response)
+    if (response.ok === true) {
+      const fetchedData = await response.json()
+      //   console.log(fetchedData.products)
+      const updatedData = fetchedData.products.map(product => ({
+        title: product.title,
+        brand: product.brand,
+        price: product.price,
+        id: product.id,
+        imageUrl: product.image_url,
+        rating: product.rating,
+      }))
+      this.setState({productsList: updatedData, isLoading: false})
+    }
+  }
+
+  renderLoader = () => (
+    <div className="loader" data-testid="loader">
+      <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
+    </div>
+  )
+
+  renderProductsList = () => {
+    const {productsList} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    const {isLoading} = this.state
+
+    return <>{isLoading ? this.renderLoader() : this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
